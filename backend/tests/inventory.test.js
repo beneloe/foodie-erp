89b80d2f7c2364d8d6d377b8d6749c13cd3eca03
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2024-06-04 22:38:26 +0200

    fix: fix the db configuration for inventory.test.js to work

--- backend/tests/inventory.test.js
+++ backend/tests/inventory.test.js
@@ -1,12 +1,25 @@
 const request = require('supertest');
 const app = require('../server');
+const { pool } = require('../config/db');
+require('dotenv').config();
 
 describe('POST /api/inventory/add', () => {
+  beforeAll(async () => {
+    // Make sure pool is connected
+    await pool.connect();
+  });
+
+  afterAll(async () => {
+    // Make sure pool is closed
+    await pool.end();
+  });
+
   it('should create a new inventory item', async () => {
     const response = await request(app)
       .post('/api/inventory/add')
       .send({ item_name: 'Bread', stock: 30000, unit: 'grams', price: 0.001, starting_quantity: 20000, picture: 'picture.jpeg' });
-    expect(response.statusCode).toBe(201);
-    expect(response.body.item_name).toBe('Tomato');
-  });
+
+    expect(response.status).toBe(201);
+    expect(response.body.item_name).toBe('Bread');
+  }, 5000);
 });
---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    setupFiles: ['dotenv/config'],
+    testEnvironment: 'node',
+  };
+  
