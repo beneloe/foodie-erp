e94c07337104f672c1e813a729778cc426bc53b1
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2024-06-06 18:07:04 +0200

    feat: add routes to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -6,13 +5,19 @@ const helmet = require('helmet');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-const inventory = require('./routes/inventory');
+const inventoryRoutes = require('./routes/inventory');
+const purchaseOrderRoutes = require('./routes/purchaseOrders');
+const productionOrderRoutes = require('./routes/productionOrders');
+const salesOrderRoutes = require('./routes/salesOrders');
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(helmet());
 
-app.use('/api/inventory', inventory);
+app.use('/api/inventory', inventoryRoutes);
+app.use('/api/purchase-orders', purchaseOrderRoutes);
+app.use('/api/production-orders', productionOrderRoutes);
+app.use('/api/sales-orders', salesOrderRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is working');
