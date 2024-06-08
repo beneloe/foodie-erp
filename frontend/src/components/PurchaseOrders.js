75c1c5d84cc15aa00d52a779fe322121f1b44818
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2024-06-08 23:04:17 +0200

    feat: add new routes and links to view files

--- frontend/src/components/PurchaseOrders.js
+++ frontend/src/components/PurchaseOrders.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const PurchaseOrders = () => {
   const [purchaseOrders, setPurchaseOrders] = useState([]);
@@ -53,6 +54,7 @@ const PurchaseOrders = () => {
           ))}
         </tbody>
       </table>
+      <Link to="/purchases/create">Add New</Link>
     </div>
   );
 };
