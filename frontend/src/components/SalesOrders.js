85ff474f4a542ced40b419374eb07a5b14f3fd0c
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2024-06-09 15:21:42 +0200

    feat: add link to add sales order to sales order list view

--- frontend/src/components/SalesOrders.js
+++ frontend/src/components/SalesOrders.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const SalesOrders = () => {
   const [salesOrders, setSalesOrders] = useState([]);
@@ -53,6 +54,7 @@ const SalesOrders = () => {
           ))}
         </tbody>
       </table>
+      <Link to="/sales/create">Add New</Link>
     </div>
   );
 };
