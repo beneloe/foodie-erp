117f59aff115451681ce01696f0c78a505ea6121
Benedikt Löffler <78652732+beneloe@users.noreply.github.com>
2024-06-17 16:09:33 +0200

    chore: update rate limiter to higher limit

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,7 @@ app.set('trust proxy', 'loopback');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per window in 15 minutes
+  max: 10000, // limit each IP to 10000 requests per window in 15 minutes
   standardHeaders: true,
   legacyHeaders: false,
 });
